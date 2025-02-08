14189fb2887d0bf84d0fe9407133dbccde8f0a46
Someswarklizos2001 <someswar@klizos.com>
2025-02-09 01:38:44 +0530

    backend_push

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const io = require("socket.io")(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://ping-me-frontend.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
