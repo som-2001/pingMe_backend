7e8068252f65acaa3f9eafecd499c0f16559a3b6
someswar <someswar@klizos.com>
2025-02-13 12:41:24 +0530

    updated

--- routes/chat.js
+++ routes/chat.js
@@ -6,6 +6,7 @@ const upload = require("../middleware/uploadImg");
 router.post('/get-messages',getMessages);
 router.post('/get-chats',getChats);
 router.post('/upload-images',upload('pingMe/chat').single("images"),uploadImages);
+router.post('/upload-images',upload('pingMe/chat').single("videos"),uploadImages);
 router.post('/getMedia',getMedia);
 
 module.exports=router;
